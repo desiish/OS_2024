1831ef3af935db129ccd5c0fc5baaa1bf0d4fb17
Desislava Shunina <115353472+desiish@users.noreply.github.com>
2024-03-27 22:57:02 +0200

    Update 2020-SE-02.sh

--- Shell/Exams/Pipes/2020-SE-02.sh
+++ Shell/Exams/Pipes/2020-SE-02.sh
@@ -1,1 +1,2 @@
-cat spacex.txt | awk -F '|' -v cosm=$(cat spacex.txt | cut -f2,3 -d '|' | sort -k1 | cut -f1 -d ' ' | uniq -c | sort -k 1nr | head -n 1 | cut -f 1 -d ' ') '$2==cosm {print $1 " " $3 ":" $4}' | sort -k 1nr | head -n 1 | cut -f2 -d ' '
+cat spacex.txt | awk -F '|' -v cosm=$(cat spacex.txt | cut -f2,3 -d '|' | sort -k1 | cut -f1 -d ' ' |
+uniq -c | sort -k 1nr | head -n 1 | cut -f 1 -d ' ') '$2==cosm {print $1 " " $3 ":" $4}' | sort -k 1nr | head -n 1 | cut -f2 -d ' '
