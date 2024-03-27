2565ae72354eba36c85cab5231bea71ba6911f16
Desislava Shunina <115353472+desiish@users.noreply.github.com>
2024-03-27 21:58:48 +0200

    Update 2023-CE-02.sh

--- Shell/Exams/Pipes/2023-CE-02.sh
+++ Shell/Exams/Pipes/2023-CE-02.sh
@@ -1,1 +1,2 @@
-find /var/logs/my_logs -mindepth 1 -type f -regex "^[a-zA-Z_0-9][a-zA-Z_0-9]*_[0-9][0-9]*\.log$"| xargs -I{} grep -o  "error" {} | wc -l
+find /var/logs/my_logs -type f -printf "%f\n" | grep -e "^[a-zA-Z_0-9][a-zA-Z_0-9]*_[0-9][0-9]*\.txt$" | xargs -I{} cat {} | grep -o "error" | wc -l
+
