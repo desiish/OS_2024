281d536a43e16d6dfd04a005b001f09d90080655
Desislava Shunina <115353472+desiish@users.noreply.github.com>
2024-03-28 10:09:01 +0200

    Update 2019-SE-02.sh

--- Shell/Exams/Pipes/2019-SE-02.sh
+++ Shell/Exams/Pipes/2019-SE-02.sh
@@ -1,2 +1,3 @@
-find /home/SI -type d 2>/dev/null -printf "%T@ %u\n" | awk -F " " '$1>1551186000&&$1<1551176000 {print $2}' | xargs -I{} grep {} /etc/passwd
-| cut -d ':' -f 1,5 | cut -d ',' -f 1 | cut -c 2-
+find /home/SI -type d 2>/dev/null -printf "%T@ %u\n" | awk -F " " '$1>1551186000 && $1<1551176000 {print $2}' |
+xargs -I{} grep {} /etc/passwd | cut -d ':' -f 1,5 | cut -d ',' -f 1 | cut -c 2-
+
