e10343032eab57e037f6a98aff77a5edf45043a3
Desislava Shunina <115353472+desiish@users.noreply.github.com>
2024-06-13 16:52:36 +0300

    Update 2024-SE-2.sh

--- Shell/Exams/Scripts/2024-SE-2.sh
+++ Shell/Exams/Scripts/2024-SE-2.sh
@@ -13,18 +13,18 @@ pu_arr=$(./occ user:list | grep "user_id" | cut -d ':' -f2 | tr -d ' ')
 
 for user in ${lu_arr[@]}; do
         if echo "$pu" | grep -q "$user"; then
-                if echo $(./occ user:info $user) | grep -q "disable"
-                        ./occ user:enable $user
+                if echo $(./occ user:info "$user") | grep -q "disable"
+                        ./occ user:enable "$user"
                 fi
         else
-                ./occ user:add $user
+                ./occ user:add "$user"
         fi
 done
 
 for user in ${pu_arr[@]}; do
         if ! echo "$lu" | grep -q "$user"; then
-                if echo $(./occ user:info) | grep -q "enable"; then
-                        ./occ user:disable $user
+                if echo $(./occ user:info "$user") | grep -q "enable"; then
+                        ./occ user:disable "$user"
                 fi
         fi
 done
